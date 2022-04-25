97b0a0a72df0a83b885e627a81117cedb17a2a64
ns319 <77340399+ns319@users.noreply.github.com>
2022-04-25 16:16:58 -0400

    Change some comments

--- RemoveSkype_OU.ps1
+++ RemoveSkype_OU.ps1
@@ -4,20 +4,20 @@
 .SYNOPSIS
     Stop Skype for Business from launching at startup
 .DESCRIPTION
-    Prompt for an OU and delete various registry keys from HKLM where Skype/Lync may still exist, remove the Lync entry from the Run key for the current user, and delete .exe and .lnk files from
-    shared locations: Office ProgramFiles, and StartUp ProgramData. To completely remove Skype we need to delete the Lync reg entry for each user, so get a list of all users and
-    send it to the 'foreach' command, load each reg hive from NTUSER.DAT, remove the entry, then unload the hive.
+    Prompt for an OU and delete various registry keys from HKLM where Skype/Lync may still exist, remove the Lync entry from the Run key for the current user, and delete .exe and
+    .lnk files from shared locations: Office ProgramFiles, and StartUp ProgramData. To completely remove Skype we need to delete the Lync reg entry for each user, so get a list of
+    all users and send it to the 'foreach' command, load each reg hive from NTUSER.DAT, remove the entry, then unload the hive.
 .NOTES
-    I could not splat the User names from any directory other than C:\Users. No idea why; I probably missed something. But using Set-Location first works fine as a workaround.
+    I could not work with the User names from any directory other than C:\Users. No idea why; I probably missed something. But using Set-Location first works fine as a workaround.
     
     HKEY_USERS is not a default PSDrive, so we need to add it before we can use it.
 
     !-----! !-----! IMPORTANT !-----! !-----!
-    This script WILL generate errors. I loaded NTUSER.DAT from every profile listed in C:\Users, which includes profiles like DOL_DA_USER, Default, etc. These profiles
-    don't have entries for Lync because they have never run Lync/Skype, so you'll see errors to that effect. I removed 'Public' from the UserList to avoid the Access Denied
-    error when trying to unload the reg hive; I think this is because the Remove-Item command opens a handle on the hive that doesn't get closed because none of the keys
-    exist in this hive, so reg.exe can't unload the hive until the handle is closed. I could force the handle to close, but it's easier to just not open it to begin with.
-    v1.4.3
+    This script WILL generate errors. I loaded NTUSER.DAT from every profile listed in C:\Users, which includes profiles that have never run Lync/Skype, so you'll see errors to
+    that effect. I removed 'Public' from the UserList to avoid the Access Denied error when trying to unload the reg hive; I think this is because the Remove-Item command opens a
+    handle on the hive that doesn't get closed because none of the keys exist in this hive, so reg.exe can't unload the hive until the handle is closed. I could force the handle to
+    close, but it's easier to just not open it to begin with.
+    v1.4.4
 #>
 
 # Elevate to admin
@@ -55,9 +55,8 @@ foreach ($HostName in $HostList) {
         Write-Host "Remove OfficeRoot .exe from $HostName" -ForegroundColor Cyan
         Remove-Item -Path "$env:ProgramFiles\Microsoft Office\root\Office16\lync.exe" -Verbose
         
-        # This only worked for me from the C:\Users directory; anywhere else and the command would fail. Idk why?
         Set-Location -Path 'C:\Users'
-
+        # This only worked for me from the C:\Users directory; anywhere else and the command would fail. Idk why?
         $UserList = @(Get-ChildItem -Path C:\Users | Where-Object -Property Name -ne 'Public').Name
 
         # HKU: is not a default PSDrive, so add it
